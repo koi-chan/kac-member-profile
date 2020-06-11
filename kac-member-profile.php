dbe277db38e1b4af05986d08b49b6b6a50c82e89
koi-chan <koi-chan@mail.kazagakure.net>
2020-06-11 22:04:46 +0900

    first commit

---
+++ kac-member-profile.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name:		KAC Member Profile
+ * Plugin URI:		
+ * Description:		make member profile page
+ * Version:		0.0.1
+ * Author:		koi-chan
+ * Author URI:		https://twitter.com/koichan779
+ * License:		GPL2
+ */
+?>
